fe7d74fa41f637490f683755511cfc1cf96e70a3
croxydercoder <63309489+CROXY04@users.noreply.github.com>
2024-04-30 14:56:45 +0200

    tfgzhu

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Hi")
-hallo max
+
+#US1 Newton
+
+def button():
+    dp = input_dp.get().replace(",", ".")#deltaP(Impuls)
+    dm = input_dm.get().replace(",", ".")#deltaMasse
+    ve = input_ve.get().replace(",", ".")#Geschwindigkeit der ausgestoßenen Gase relativ zur Rakete
